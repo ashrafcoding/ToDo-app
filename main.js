484ba18dd9ad90a7ba2d45c236fb59709f27fc95
ashraf <ashrafeid@outlook.com>
2021-04-19 02:10:10 +0300

    add calculate function to count the tasks

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 let theInput = document.querySelector('.add-task input'),
     theAddButton = document.querySelector('.add-task .plus'),
     tasksContainer = document.querySelector('.tasks-content'),
-    noTasksMsg = document.querySelector('.no-tasks-message'),
     tasksCount = document.querySelector('.tasks-count span'),
     tasksCompleted = document.querySelector('.completed-tasks span')
 
@@ -13,8 +12,11 @@ theAddButton.onclick = function(){
     if(theInput.value === ''){
         console.log('no value')
     }else{
-        console.log(theInput.value)
-        noTasksMsg.remove()
+        let noTasksMsg = document.querySelector('.no-tasks-message')
+        if(document.body.contains(document.querySelector('.no-tasks-message'))){
+            noTasksMsg.remove()
+        }
+        console.log(theInput.value)        
         let mainSpan = document.createElement('span')
         let deleteSpan = document.createElement('span')
         let text = document.createTextNode(theInput.value)
@@ -27,14 +29,32 @@ theAddButton.onclick = function(){
         tasksContainer.appendChild(mainSpan)
         theInput.value = ''
         theInput.focus()
+        calculateTasks()
     }
 }
 
 document.addEventListener('click',function(e){
     if(e.target.className == 'delete'){
-        e.target.parentNode.remove()
+        e.target.parentNode.remove();
+        if(tasksContainer.childElementCount == 0){
+            createNoTasks()
+        }
     }
-    if(e.target.className == 'task-box'){
+    if(e.target.classList.contains('task-box')){
         e.target.classList.toggle('finished')
     }
+    calculateTasks()
 })
+
+function createNoTasks(){
+    let msgSpan = document.createElement('span')
+    let msgText = document.createTextNode('No Tasks To Show')
+    msgSpan.appendChild(msgText)
+    msgSpan.className = 'no-tasks-message'
+    tasksContainer.appendChild(msgSpan)
+}
+
+function calculateTasks(){
+    tasksCount.innerHTML = document.querySelectorAll('.tasks-content .task-box').length
+    tasksCompleted.innerHTML = document.querySelectorAll('.tasks-content .finished').length
+}
